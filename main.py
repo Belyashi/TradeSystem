cd1984d2540a51721059757bd1319b016b560e75
Alexej Lapotko <alexej.lapotko@gmail.com>
2016-11-26 20:08:33 +0300

    Created models

---
+++ main.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+
+from models import Base
+
+
+def main():
+    engine = create_engine('sqlite:///:memory:', echo=True)
+    Base.metadata.create_all(engine)
+
+
+if __name__ == '__main__':
+    main()
