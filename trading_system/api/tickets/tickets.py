0b0ae116a25e59c1d74645333fbc2fd853af375b
shmatkovd <client.pas.95@gmail.com>
2016-12-28 01:14:11 +0300

    Fix.

--- trading_system/api/tickets/tickets.py
+++ trading_system/api/tickets/tickets.py
@@ -7,4 +7,4 @@ tickets = Blueprint('tickets', __name__, url_prefix='/tickets')
 tickets.before_request(auth_middleware)
 
 tickets.add_url_rule('/', view_func=TicketsController.as_view('tickets_view'), methods=['GET', 'POST'])
-tickets.add_url_rule('/<int:ticket_id>', view_func=TicketController('ticket_view'), methods=['GET', 'DELETE'])
+tickets.add_url_rule('/<int:ticket_id>', view_func=TicketController.as_view('ticket_view'), methods=['GET', 'DELETE'])
